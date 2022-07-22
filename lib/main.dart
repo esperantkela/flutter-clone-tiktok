12f859e87e01e7bce43a9b5d6c7ed40e95fb8108
Espérant KELA <esperant.kela@gmail.com>
2022-07-22 07:53:07 +0100

    change police to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tiktok_clone/screens/home_screen.dart';
 
 void main() {
@@ -14,8 +15,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(
-  
-        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.notoSansAdlamTextTheme()
       ),
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
