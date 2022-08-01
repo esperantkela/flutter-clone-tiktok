ef4bbbda1a067305823414abcf1d179a6aa3cbc4
Espérant KELA <esperant.kela@gmail.com>
2022-08-01 15:18:54 +0100

    create avatar widgets

--- lib/widgets/avatar_widget.dart
+++ lib/widgets/avatar_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 
 class AvatarWidget extends StatelessWidget {
   const AvatarWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
+    return Container();
   }
 }
