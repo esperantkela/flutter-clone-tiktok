30bf09efcd39b5e9b0a76b2964f361172f9b9f76
Espérant KELA <esperant.kela@gmail.com>
2022-08-01 15:54:07 +0100

    circulavatar

--- lib/widgets/avatar_widget.dart
+++ lib/widgets/avatar_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
@@ -7,6 +8,12 @@ class AvatarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CircleAvatar(
+      radius: 30.0,
+      backgroundImage: AssetImage(
+        "assets/profile/profile-1.jpg",
+      )
+
+    );
   }
 }
