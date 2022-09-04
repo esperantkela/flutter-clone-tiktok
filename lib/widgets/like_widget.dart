9221074485c68893f2776da608d97b3fc8269ce8
Espérant KELA <esperant.kela@gmail.com>
2022-09-04 09:52:37 +0100

    finished like module

--- lib/widgets/like_widget.dart
+++ lib/widgets/like_widget.dart
@@ -8,26 +8,45 @@ class LikeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 10.0,
-          sigmaY: 10.0
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white.withOpacity(0.1)
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 8.0,
           ),
-          child:  Center(
-            child: IconButton(
-              onPressed: (){} ,
-              icon: const Icon(Icons.favorite_border_rounded),
-              color: Colors.white,
+          child: ClipOval(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 10.0,
+                sigmaY: 10.0
               ),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white.withOpacity(0.1)
+                ),
+                child:  Center(
+                  child: IconButton(
+                    onPressed: (){} ,
+                    icon: const Icon(Icons.favorite_border_rounded),
+                    color: Colors.white,
+                    iconSize: 25.0,
+                    ),
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+        const Text(
+          "1.6K",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: .8
+          ),
+          
+        ),
+      ],
     );
   }
 }
